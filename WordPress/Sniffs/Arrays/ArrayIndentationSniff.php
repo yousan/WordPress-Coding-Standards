a3fcb4685ce824edf936888abef0fa4c9c5b6ffb
jrfnl <jrfnl@users.noreply.github.com>
2017-06-19 08:01:06 +0200

    :bug: ArrayIndentation sniff: various improvements to the reporting & fixing
    The ArrayIndentation sniff did not handle multi-line array items. The first line would be handled, the subsequent lines were previously disregarded. This has now been fixed.
    
    The fix takes into account that subsequent lines may have varying indentation, for instance to indicate HTML layout.
     > Subsequent lines may be indented more or less than the mimimum expected indent,  but the "first line after" should be indented - at least - as much as the very first line of the array item.
    Indentation correction for subsequent lines will be based on that diff.
    (inline comment in sniff)
    
    Fixes 973
    
    ------
    
    While working on the above, I realized that heredoc/nowdoc tokens - except for the opener - should be disregarded from indentation fixes by this sniff as it could break the closer and distort the text layout in a heredoc/nowdoc.
    
    This has now been sorted as well.
    
    ------
    
    Lastly, arrays interspersed with comments, both for single line items as well as multi-line items, were not being aligned properly.
    
    This has also been fixed.
    
    Fixes 985
    
    ------
    
    I've renamed a number of internally used variables to make it easier to distinguish between them.
    I've also split of parts of the code into separate methods to prevent code duplication.
    The `determine_whitespace()` and `spaces_to_indent()` methods - and possibly even the `fix_array_alignment_error()` method - can be regarded as utility methods and could/should in the future be moved to the `WordPress_Sniff` class if & when other sniffs would need them.

--- WordPress/Sniffs/Arrays/ArrayIndentationSniff.php
+++ WordPress/Sniffs/Arrays/ArrayIndentationSniff.php
@@ -25,6 +25,15 @@ class WordPress_Sniffs_Arrays_ArrayIndentationSniff extends WordPress_Sniff {
 	 */
 	private $tab_width;
 
+	/**
+	 * Tokens to ignore for subsequent lines in a multi-line array item.
+	 *
+	 * Property is set in the register() method.
+	 *
+	 * @var array
+	 */
+	private $ignore_tokens = array();
+
 
 	/**
 	 * Returns an array of tokens this test wants to listen for.
@@ -32,6 +41,10 @@ class WordPress_Sniffs_Arrays_ArrayIndentationSniff extends WordPress_Sniff {
 	 * @return array
 	 */
 	public function register() {
+		// Set the $ignore_tokens property.
+		$this->ignore_tokens = PHP_CodeSniffer_Tokens::$heredocTokens;
+		unset( $this->ignore_tokens[ T_START_HEREDOC ], $this->ignore_tokens[ T_START_NOWDOC ] );
+
 		return array(
 			T_ARRAY,
 			T_OPEN_SHORT_ARRAY,
@@ -111,82 +124,299 @@ class WordPress_Sniffs_Arrays_ArrayIndentationSniff extends WordPress_Sniff {
 		 * containing the array opener.
 		 */
 		if ( $this->tokens[ $closer ]['column'] !== $column ) {
-			$expected = ( $column - 1 );
-			$found    = ( $this->tokens[ $closer ]['column'] - 1 );
-			$error    = 'Array closer not aligned correctly; expected %s space(s) but found %s';
-			$data     = array(
-				$expected,
-				$found,
-			);
 
-			$fix = $this->phpcsFile->addFixableError( $error, $closer, 'CloseBraceNotAligned', $data );
-			if ( true === $fix ) {
-				if ( 0 === $found ) {
-					$this->phpcsFile->fixer->addContent( ( $closer - 1 ), $indentation );
-				} else {
-					$this->phpcsFile->fixer->replaceToken( ( $closer - 1 ), $indentation );
-				}
-			}
+			$this->add_array_alignment_error(
+				$closer,
+				'Array closer not aligned correctly; expected %s space(s) but found %s',
+				'CloseBraceNotAligned',
+				( $column - 1 ),
+				( $this->tokens[ $closer ]['column'] - 1 ),
+				$indentation
+			);
 		}
 
+		/*
+		 * Verify & correct the array item indentation.
+		 */
 		$array_items = $this->get_function_call_parameters( $stackPtr );
 		if ( empty( $array_items ) ) {
 			// Strange, no array items found.
 			return;
 		}
 
-		$expected_indent  = "\t" . $indentation;
-		$expected_column  = ( $column + $this->tab_width );
-		$end_of_last_item = $opener;
+		$expected_indent      = "\t" . $indentation;
+		$expected_spaces      = ( ( $column + $this->tab_width ) - 1 );
+		$end_of_previous_item = $opener;
 
 		foreach ( $array_items as $item ) {
 			// Find the line on which the item starts.
-			$first_content = $this->phpcsFile->findNext( PHP_CodeSniffer_Tokens::$emptyTokens, $item['start'], ( $item['end'] + 1 ), true );
+			$first_content    = $this->phpcsFile->findNext( array( T_WHITESPACE, T_DOC_COMMENT_WHITESPACE ), $item['start'], ( $item['end'] + 1 ), true );
+			$end_of_this_item = ( $item['end'] + 1 );
 			if ( false === $first_content ) {
-				$end_of_last_item = ( $item['end'] + 1 );
+				$end_of_previous_item = $end_of_this_item;
 				continue;
 			}
 
 			// Bow out from reporting and fixing mixed multi-line/single-line arrays.
 			// That is handled by the ArrayDeclarationSniff.
-			if ( $this->tokens[ $first_content ]['line'] === $this->tokens[ $end_of_last_item ]['line'] ) {
+			if ( $this->tokens[ $first_content ]['line'] === $this->tokens[ $end_of_previous_item ]['line'] ) {
 				return $closer;
 			}
 
-			$whitespace = '';
-			if ( 1 !== $this->tokens[ $first_content ]['column'] ) {
-				$whitespace = $this->tokens[ ( $first_content - 1 ) ]['content'];
+			$found_indent = $this->determine_whitespace( $first_content );
+			$found_spaces = ( $this->tokens[ $first_content ]['column'] - 1 );
 
-				// If tabs are being converted to spaces by the tokenizer, the
-				// original content should be checked instead of the converted content.
-				if ( isset( $this->tokens[ ( $first_content - 1 ) ]['orig_content'] ) ) {
-					$whitespace = $this->tokens[ ( $first_content - 1 ) ]['orig_content'];
+			if ( $found_indent !== $expected_indent ) {
+				$this->add_array_alignment_error(
+					$first_content,
+					'Array item not aligned correctly; expected %s spaces but found %s',
+					'ItemNotAligned',
+					$expected_spaces,
+					$found_spaces,
+					$expected_indent
+				);
+			}
+
+			// No need for further checking if this is a one-line array item.
+			if ( $this->tokens[ $first_content ]['line'] === $this->tokens[ $item['end'] ]['line'] ) {
+				$end_of_previous_item = $end_of_this_item;
+				continue;
+			}
+
+			/*
+			 * Multi-line array item. Verify & if needed, correct the indentation of subsequent lines.
+			 * Subsequent lines may be indented more or less than the mimimum expected indent,
+			 * but the "first line after" should be indented - at least - as much as the very first line
+			 * of the array item.
+			 * Indentation correction for subsequent lines will be based on that diff.
+			 */
+
+			// Find first content on second line of the array item.
+			// If the second line is a heredoc/nowdoc, continue on until we find a line with a different token.
+			for ( $ptr = ( $first_content + 1 ); $ptr <= $item['end']; $ptr++ ) {
+				if ( $this->tokens[ $first_content ]['line'] !== $this->tokens[ $ptr ]['line']
+					&& ! isset( $this->ignore_tokens[ $this->tokens[ $ptr ]['code'] ] )
+				) {
+					break;
 				}
 			}
 
-			if ( $whitespace !== $expected_indent ) {
-				$expected = ( $expected_column - 1 );
-				$found    = ( $this->tokens[ $first_content ]['column'] - 1 );
-				$error    = 'Array item not aligned correctly; expected %s spaces but found %s';
-				$data     = array(
-					$expected,
-					$found,
+			if ( false === $ptr ) {
+				$end_of_previous_item = $end_of_this_item;
+				continue;
+			}
+
+			$first_content_on_line2 = $this->phpcsFile->findNext( array( T_WHITESPACE, T_DOC_COMMENT_WHITESPACE ), $ptr, ( $item['end'] + 1 ), true );
+
+			if ( false === $first_content_on_line2 ) {
+				$end_of_previous_item = $end_of_this_item;
+				continue;
+			}
+
+			$found_indent_on_line2    = $this->determine_whitespace( $first_content_on_line2 );
+			$found_spaces_on_line2    = strlen(
+				str_replace( "\t", str_repeat( ' ', $this->tab_width ), $found_indent_on_line2 )
+			); // Can't rely on column for space determination because of multi-line non-docblock comments.
+			$line2_indent_diff        = 0;
+			$expected_spaces_on_line2 = $expected_spaces;
+
+			if ( $found_spaces < $found_spaces_on_line2 ) {
+				$line2_indent_diff        = ( $found_spaces_on_line2 - $found_spaces );
+				$expected_spaces_on_line2 = $expected_spaces + $line2_indent_diff;
+			}
+			$expected_indent_on_line2 = $this->spaces_to_indent( $expected_spaces_on_line2 );
+
+			if ( $found_indent_on_line2 !== $expected_indent_on_line2 ) {
+
+				$fix = $this->phpcsFile->addFixableError(
+					'Multi-line array item not aligned correctly; expected %s spaces, but found %s',
+					$first_content_on_line2,
+					'MultiLineArrayItemNotAligned',
+					array(
+						$expected_spaces_on_line2,
+						$found_spaces_on_line2,
+					)
 				);
 
-				$fix = $this->phpcsFile->addFixableError( $error, $first_content, 'ItemNotAligned', $data );
 				if ( true === $fix ) {
-					if ( 0 === $found ) {
-						$this->phpcsFile->fixer->addContent( ( $first_content - 1 ), $expected_indent );
+					$this->phpcsFile->fixer->beginChangeset();
+
+					// Fix second line for the array item.
+					if ( 1 === $this->tokens[ $first_content_on_line2 ]['column']
+						&& T_COMMENT === $this->tokens[ $first_content_on_line2 ]['code']
+					) {
+						$actual_comment = ltrim( $this->tokens[ $first_content_on_line2 ]['content'] );
+						$replacement    = $expected_indent_on_line2 . $actual_comment;
+
+						$this->phpcsFile->fixer->replaceToken( $first_content_on_line2, $replacement );
+
 					} else {
-						$this->phpcsFile->fixer->replaceToken( ( $first_content - 1 ), $expected_indent );
+						$this->fix_alignment_error( $first_content_on_line2, $expected_indent_on_line2 );
 					}
+
+					// Fix subsequent lines.
+					for ( $i = ( $first_content_on_line2 + 1 ); $i <= $item['end']; $i++ ) {
+						// We're only interested in the first token on each line.
+						if ( 1 !== $this->tokens[ $i ]['column'] ) {
+							if ( $this->tokens[ $i ]['line'] === $this->tokens[ $item['end'] ]['line'] ) {
+								// We might as well quit if we're past the first token on the last line.
+								break;
+							}
+							continue;
+						}
+
+						$first_content_on_line = $this->phpcsFile->findNext(
+							array( T_WHITESPACE, T_DOC_COMMENT_WHITESPACE ),
+							$i,
+							( $item['end'] + 1 ),
+							true
+						);
+
+						if ( false === $first_content_on_line ) {
+							break;
+						}
+
+						// Ignore lines with heredoc and nowdoc tokens.
+						if ( isset( $this->ignore_tokens[ $this->tokens[ $first_content_on_line ]['code'] ] ) ) {
+							$i = $first_content_on_line;
+							continue;
+						}
+
+						$found_indent_on_line = $this->determine_whitespace( $first_content_on_line );
+						$found_spaces_on_line = strlen(
+							str_replace( "\t", str_repeat( ' ', $this->tab_width ), $found_indent_on_line )
+						);
+
+						$expected_spaces_on_line = ( $expected_spaces_on_line2 + ( $found_spaces_on_line - $found_spaces_on_line2 ) );
+						$expected_spaces_on_line = max( $expected_spaces_on_line, 0 ); // Can't be below 0.
+						$expected_indent_on_line = $this->spaces_to_indent( $expected_spaces_on_line );
+
+						if ( $found_indent_on_line !== $expected_indent_on_line ) {
+							if ( 1 === $this->tokens[ $first_content_on_line ]['column']
+								&& T_COMMENT === $this->tokens[ $first_content_on_line ]['code']
+							) {
+								$actual_comment = ltrim( $this->tokens[ $first_content_on_line ]['content'] );
+								$replacement    = $expected_indent_on_line . $actual_comment;
+
+								$this->phpcsFile->fixer->replaceToken( $first_content_on_line, $replacement );
+							} else {
+								$this->fix_alignment_error( $first_content_on_line, $expected_indent_on_line );
+							}
+						}
+
+						// Move passed any potential empty lines between the previous item and this one.
+						// No need to do the fixes twice.
+						$i = $first_content_on_line;
+					}
+
+					$this->phpcsFile->fixer->endChangeset();
 				}
 			}
 
-			$end_of_last_item = ( $item['end'] + 1 );
-
+			$end_of_previous_item = $end_of_this_item;
 		}
 
 	} // End process_token().
+
+
+	/**
+	 * Determine the line indentation whitespace.
+	 *
+	 * @param int $ptr Stack pointer to the first non-whitespace token on the line.
+	 *
+	 * @return string
+	 */
+	protected function determine_whitespace( $ptr ) {
+		$whitespace = '';
+		if ( 1 !== $this->tokens[ $ptr ]['column']
+			&& ( T_WHITESPACE === $this->tokens[ ( $ptr - 1 ) ]['code']
+				|| T_DOC_COMMENT_WHITESPACE === $this->tokens[ ( $ptr - 1 ) ]['code'] )
+		) {
+			$whitespace = $this->tokens[ ( $ptr - 1 ) ]['content'];
+
+			// If tabs are being converted to spaces by the tokenizer, the
+			// original content should be checked instead of the converted content.
+			if ( isset( $this->tokens[ ( $ptr - 1 ) ]['orig_content'] ) ) {
+				$whitespace = $this->tokens[ ( $ptr - 1 ) ]['orig_content'];
+			}
+		}
+
+		/*
+		 * Special case for multi-line, non-docblock comments.
+		 * Only applicable for subsequent lines in an array item.
+		 *
+		 * First/Single line is tokenized as T_WHITESPACE + T_COMMENT
+		 * Subsequent lines are tokenized as T_COMMENT including the indentation whitespace.
+		 */
+		if ( 1 === $this->tokens[ $ptr ]['column']
+			&& T_COMMENT === $this->tokens[ $ptr ]['code']
+		) {
+			$content = $this->tokens[ $ptr ]['content'];
+			if ( isset( $this->tokens[ $ptr ]['orig_content'] ) ) {
+				$content = $this->tokens[ $ptr ]['orig_content'];
+			}
+
+			$actual_comment = ltrim( $content );
+			$whitespace     = str_replace( $actual_comment, '', $content );
+		}
+
+		return $whitespace;
+	}
+
+	/**
+	 * Create a tab-based indentation string.
+	 *
+	 * @param int $nr Number of "spaces" the indentation should be.
+	 *
+	 * @return string
+	 */
+	protected function spaces_to_indent( $nr ) {
+		if ( 0 >= $nr ) {
+			return '';
+		}
+
+		$num_tabs  = (int) floor( $nr / $this->tab_width );
+		$remaining = ( $nr % $this->tab_width );
+		$indent    = str_repeat( "\t", $num_tabs );
+		$indent   .= str_repeat( ' ', $remaining );
+
+		return $indent;
+	}
+
+	/**
+	 * Throw an error and fix incorrect array alignment.
+	 *
+	 * @param int    $ptr        Stack pointer to the first content on the line.
+	 * @param string $error      Error message.
+	 * @param string $error_code Error code.
+	 * @param int    $expected   Expected nr of spaces (tabs translated to space value).
+	 * @param int    $found      Found nr of spaces (tabs translated to space value).
+	 * @param string $new_indent Whitespace indent replacement value.
+	 */
+	protected function add_array_alignment_error( $ptr, $error, $error_code, $expected, $found, $new_indent ) {
+		$data  = array(
+			$expected,
+			$found,
+		);
+
+		$fix = $this->phpcsFile->addFixableError( $error, $ptr, $error_code, $data );
+		if ( true === $fix ) {
+			$this->fix_alignment_error( $ptr, $new_indent );
+		}
+	}
+
+	/**
+	 * Fix incorrect array alignment.
+	 *
+	 * @param int    $ptr        Stack pointer to the first content on the line.
+	 * @param string $new_indent Whitespace indent replacement value.
+	 */
+	protected function fix_alignment_error( $ptr, $new_indent ) {
+		if ( 0 === ( $this->tokens[ $ptr ]['column'] - 1 ) ) {
+			$this->phpcsFile->fixer->addContent( ( $ptr - 1 ), $new_indent );
+		} else {
+			$this->phpcsFile->fixer->replaceToken( ( $ptr - 1 ), $new_indent );
+		}
+	}
 
 } // End class.
